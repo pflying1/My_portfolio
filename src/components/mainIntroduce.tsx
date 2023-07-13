9f88d7cc889e23e5e958ce293ed860c958911df8
Jeong Seong Cheol <tjdcjf9333@gamil.com>
2023-07-13 17:36:41 +0900

    소개 수정

--- src/components/mainIntroduce.tsx
+++ src/components/mainIntroduce.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const App: React.FC = () => {
   return (
     <div className='mainNavCss'>
-      <div>안녕하세요. developer 정성철입니다.<br></br>저희집에 방문해 주셔서 감사합니다. </div>
+      <div>안녕하세요. developer 정성철입니다.<br></br>제 집에 방문해 주셔서 감사합니다. </div>
     </div>
   );
 };
