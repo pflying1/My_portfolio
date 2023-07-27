8a767ec9b226c4bc3fd4ea51dd12d56d6d368e9f
jeongseongcheol <tjdcjf9333@gmail.com>
2023-07-27 16:59:20 +0900

    main issue5 머지 테스트

--- src/containers/favorite.tsx
+++ src/containers/favorite.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Logo from "../components/logo"
 import "../styles/favorite.css"
 import FavoriteIntro from "../components/favoriteIntro"
-import FavoriteBody from "../components/favoriteBody";
+import FavoriteBody from "../components/favoritebody";
 
 const main:React.FC = () => {
   return (
