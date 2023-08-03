267e50cb3714ffe75843175eda0bdf2c3c12b008
jeongseongcheol <tjdcjf9333@gmail.com>
2023-08-03 21:25:17 +0900

    타이틀 추가

---
+++ src/components/learnAndWriteIntro.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const favoriteIntro: React.FC = () => {
+  return (
+    <div className='favoriteIntroCss'>
+      <br />
+      <br />
+      <div className='favoriteIntroWrapCss'>
+        <div className='favoriteIntroTextCss'>배움과 기록의 방</div>
+        <div className='favoriteIntroTextCss'>방</div>
+      </div>
+    </div>
+  );
+};
+
+export default favoriteIntro;
--- src/containers/learnAndWrite.tsx
+++ src/containers/learnAndWrite.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Logo from "../components/logo"
 import "../styles/learnAndWrite.css"
+import LearnAndWriteIntro from "../components/learnAndWriteIntro"
 
 const learnAndWrite:React.FC = () => {
   return (
     <div className="learnAndWriteCss">
       <Logo />
+      <LearnAndWriteIntro />
     </div>
   )
 }
